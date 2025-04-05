4b242c7291dd3a9bbe3b94e33ee15ef56aa0ccd2
willll <464311+willll@users.noreply.github.com>
2025-04-05 14:16:22 -0400

    feat(tests): Add run_tests_mednafen.bat script for Linux and Windows environments

---
+++ Tests/run_tests_mednafen.bat
@@ -0,0 +1,33 @@
+
+@goto(){
+  # Linux code here
+  #command="kronos -a -ns -i BuildDrop/UTs.cue"
+  export SDL_VIDEODRIVER=dummy
+  command="mednafen -force_module ss BuildDrop/UTs.cue"
+  log="uts.log"
+  match="***UT_END***"
+
+  $command 2>&1 | tee "$log" &
+
+  while sleep 1
+  do
+      if fgrep --quiet "$match" "$log"
+      then
+          pkill -9 kronos
+          exit 0
+      fi
+  done
+}
+
+@goto $@
+exit
+
+:(){
+  @echo off
+  rem Windows script here
+  echo "Some MS Windows foos required here"
+
+  )
+  GOTO end
+
+  :end
