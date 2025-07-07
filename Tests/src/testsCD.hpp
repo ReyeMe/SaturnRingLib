069d9af24592181f6a12888acf33785b40f2a40f
ReyeMe <24783344+ReyeMe@users.noreply.github.com>
2025-07-07 21:36:13 +0200

    fix(NUNIT): Fixed CD tests

--- Tests/src/testsCD.hpp
+++ Tests/src/testsCD.hpp
@@ -299,7 +299,11 @@ extern "C"
     // Test: Verify seeking to the beginning of a file.
     MU_TEST(cd_file_seek_test_beginning)
     {
+        const char *dirname = "ROOT";
         const char *filename = "TESTFILE.UTS";
+
+        SRL::Cd::ChangeDir(dirname);
+
         Cd::File file(filename);
 
         // Verify the file exists and can be opened
@@ -328,7 +332,11 @@ extern "C"
     // Test seeking to a specific offset
     MU_TEST(cd_file_seek_test_offset)
     {
+        const char *dirname = "ROOT";
         const char *filename = "TESTFILE.UTS";
+
+        SRL::Cd::ChangeDir(dirname);
+
         Cd::File file(filename);
 
         bool exists = file.Exists();
@@ -357,7 +365,11 @@ extern "C"
     // Test seeking relative to the current position
     MU_TEST(cd_file_seek_test_relative)
     {
+        const char *dirname = "ROOT";
         const char *filename = "TESTFILE.UTS";
+
+        SRL::Cd::ChangeDir(dirname);
+
         Cd::File file(filename);
 
         bool exists = file.Exists();
@@ -383,7 +395,11 @@ extern "C"
     // Test seeking to an invalid offset (negative)
     MU_TEST(cd_file_seek_test_invalid_negative)
     {
+        const char *dirname = "ROOT";
         const char *filename = "TESTFILE.UTS";
+
+        SRL::Cd::ChangeDir(dirname);
+
         Cd::File file(filename);
 
         // Verify the file exists and can be opened
@@ -407,7 +423,11 @@ extern "C"
     // Test: Verify seeking to an invalid offset (beyond file size).
     MU_TEST(cd_file_seek_test_invalid_beyond)
     {
+        const char *dirname = "ROOT";
         const char *filename = "TESTFILE.UTS";
+
+        SRL::Cd::ChangeDir(dirname);
+
         Cd::File file(filename);
 
         // Verify the file exists and can be opened
