b1344fa4f0ab9bb9cc0f850a0e291d239f072d5d
willll <464311+willll@users.noreply.github.com>
2025-09-02 18:57:56 -0400

    Refactor logging classes to improve output handling and configuration

--- saturnringlib/srl_log.hpp
+++ saturnringlib/srl_log.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "srl_base.hpp"
-#include "srl_string.hpp" // for snprintf
-#include "srl_debug.hpp"  // for SRL_DEBUG_MAX_LOG_LENGTH
-#include "srl_devcart.hpp"  // for USB DevCart communication
+#include "srl_string.hpp"  // for snprintf
+#include "srl_debug.hpp"   // for SRL_DEBUG_MAX_LOG_LENGTH
+#include "srl_devcart.hpp" // for USB DevCart communication
 
 namespace SRL
 {
+
     /** @brief Logger namespace that holds the logger functionality
      * @details This class allows for writing log messages into kronos console
      */
@@ -39,6 +40,52 @@ namespace SRL
             /** @brief NONE Level, used to disable logging
              */
             NONE = 99
+        };
+
+        /** @brief LogOutputs
+         */
+        enum class LogOutputs : uint8_t
+        {
+            /** @brief DEV_CART Level, used for logging to the DevCart
+             */
+            DEV_CART = 0,
+
+            /** @brief EMULATOR Level, used for logging to the Emulator
+             */
+            EMULATOR = 1,
+
+            /** @brief NONE Level, used to disable logging
+             */
+            NONE = 99
+        };
+
+        /** @brief Log class
+         */
+        class DummyLogger
+        {
+
+        public:
+            /** @brief disable default constructor
+             */
+            DummyLogger() = delete;
+
+            /** @brief disable copy constructor
+             */
+            DummyLogger(const DummyLogger &) = delete;
+
+            /** @brief disable assignment operator
+             */
+            DummyLogger &operator=(const DummyLogger &) = delete;
+
+            static void putc(char c)
+            {
+                c;
+            }
+
+            static void putc(const char *c)
+            {
+                c;
+            }
         };
 
         /** @brief Log class
@@ -96,16 +143,63 @@ namespace SRL
              */
             DevCartLogger &operator=(const DevCartLogger &) = delete;
 
+            /** @brief Buffer size configuration
+             */
+            static constexpr size_t bufferSize = 128; // Example buffer size
+
+            /** @brief Log buffer
+             */
+
             static void putc(char c)
             {
-                putc(&c);
+                static uint8_t buffer[bufferSize];
+                static uint8_t bufferIndex = 0;
+
+                buffer[bufferIndex++] = c;
+
+                if (c == '\n' || bufferIndex == bufferSize)
+                {
+                    SRL::DevCart::CS0::write(reinterpret_cast<const uint8_t *>(const_cast<uint8_t *>(buffer)), bufferIndex);
+                    bufferIndex = 0;
+                }
             }
 
             static void putc(const char *c)
             {
-                SRL::DevCart::CS0::write(reinterpret_cast<const uint8_t *>(c));
+                while (*c)
+                {
+                    putc(*c++);
+                }
             }
         };
+
+#ifndef SRL_LOG_OUTPUT
+        /** @brief Minimum log level to be output
+         */
+        static constexpr SRL::Logger::LogOutputs LogOutput = SRL::Logger::LogOutputs::NONE;
+#else
+#define Stringify(U) SRL::Logger::LogOutputs::U
+
+        /** @brief Minimum log level to be output
+         */
+        static constexpr SRL::Logger::LogOutputs LogOutput = Stringify(SRL_LOG_OUTPUT);
+#undef Stringify
+#endif
+
+        // Compile-time type selection for Output
+        using DefaultLogger = std::conditional_t<
+            LogOutput == SRL::Logger::LogOutputs::DEV_CART, SRL::Logger::DevCartLogger,
+            std::conditional_t<
+                LogOutput == SRL::Logger::LogOutputs::EMULATOR, SRL::Logger::EmulatorLogger,
+                SRL::Logger::DummyLogger // Default case
+                >>;
+
+        // Static assertion to catch invalid SRL_LOG_OUTPUT values
+        static_assert(
+            LogOutput == SRL::Logger::LogOutputs::DEV_CART ||
+                LogOutput == SRL::Logger::LogOutputs::EMULATOR ||
+                LogOutput == SRL::Logger::LogOutputs::NONE,
+            "Invalid SRL_LOG_OUTPUT value");
 
         /** @brief Log class
          */
@@ -202,7 +296,7 @@ namespace SRL
              * @tparam lvl  Log level
              * @param message Custom message to show
              */
-            template <SRL::Logger::LogLevels lvl, typename Output = EmulatorLogger>
+            template <SRL::Logger::LogLevels lvl, typename Output = DefaultLogger>
             inline static void LogPrintInternal(const char *message)
             {
                 if constexpr (lvl >= MinLevel)
@@ -238,7 +332,7 @@ namespace SRL
              * @param message Custom message to show
              * @param args Text arguments
              */
-            template <SRL::Logger::LogLevels lvl = MinLevel, typename Output = EmulatorLogger, typename... Args>
+            template <SRL::Logger::LogLevels lvl = MinLevel, typename Output = DefaultLogger, typename... Args>
             inline static void LogPrint(const char *message, Args... args)
             {
                 if constexpr (lvl >= MinLevel)
@@ -254,7 +348,7 @@ namespace SRL
          * @param message Custom message to show
          * @param args Text arguments
          */
-        template <typename Output = EmulatorLogger, typename... Args>
+        template <typename Output = DefaultLogger, typename... Args>
         inline void LogTrace(const char *message, Args... args)
         {
             SRL::Logger::Log::LogPrint<SRL::Logger::LogLevels::TRACE, Output>(message, args...);
@@ -264,7 +358,7 @@ namespace SRL
          * @param message Custom message to show
          * @param args Text arguments
          */
-        template <typename Output = EmulatorLogger, typename... Args>
+        template <typename Output = DefaultLogger, typename... Args>
         inline void LogInfo(const char *message, Args... args)
         {
             SRL::Logger::Log::LogPrint<SRL::Logger::LogLevels::INFO, Output>(message, args...);
@@ -274,7 +368,7 @@ namespace SRL
          * @param message Custom message to show
          * @param args Text arguments
          */
-        template <typename Output = EmulatorLogger, typename... Args>
+        template <typename Output = DefaultLogger, typename... Args>
         inline void LogDebug(const char *message, Args... args)
         {
             SRL::Logger::Log::LogPrint<SRL::Logger::LogLevels::TESTING, Output>(message, args...);
@@ -284,7 +378,7 @@ namespace SRL
          * @param message Custom message to show
          * @param args Text arguments
          */
-        template <typename Output = EmulatorLogger, typename... Args>
+        template <typename Output = DefaultLogger, typename... Args>
         inline void LogWarning(const char *message, Args... args)
         {
             SRL::Logger::Log::LogPrint<SRL::Logger::LogLevels::WARNING, Output>(message, args...);
@@ -294,7 +388,7 @@ namespace SRL
          * @param message Custom message to show
          * @param args Text arguments
          */
-        template <typename Output = EmulatorLogger, typename... Args>
+        template <typename Output = DefaultLogger, typename... Args>
         inline void LogFatal(const char *message, Args... args)
         {
             SRL::Logger::Log::LogPrint<SRL::Logger::LogLevels::FATAL, Output>(message, args...);
@@ -304,7 +398,7 @@ namespace SRL
          * @param message Custom message to show
          * @param args Text arguments
          */
-        template <typename Output = EmulatorLogger, typename... Args>
+        template <typename Output = DefaultLogger, typename... Args>
         inline void LogPrint(const char *message, Args... args)
         {
             SRL::Logger::LogInfo<Output>(message, args...);
