2404a1efcb1d10646aa5ac7d6395a492ba7431da
willll <464311+willll@users.noreply.github.com>
2026-02-28 19:00:06 -0500

    feat(tests): reorganize test suite execution order for improved clarity and coverage

--- Tests/src/main.cxx
+++ Tests/src/main.cxx
@@ -253,98 +253,36 @@ int main()
 
   PushResultLine(strStart);
 
-  // Run ASCII test suite
-  RUN_AND_DISPLAY_SUITE(ascii_test_suite);
-
-  // Run angle test suite
-  RUN_AND_DISPLAY_SUITE(angle_test_suite);
-
-  // // Run CD test suite
-  RUN_AND_DISPLAY_SUITE(cd_test_suite);
-
-  // // Run CRAM test suite
-  RUN_AND_DISPLAY_SUITE(cram_test_suite);
-
-  // // Run FXP test suite
-  RUN_AND_DISPLAY_SUITE(fxp_test_suite);
-
-  // // Run HighColor test suite
-  RUN_AND_DISPLAY_SUITE(highcolor_test_suite);
-
-  // // Run Math test suite
-  RUN_AND_DISPLAY_SUITE(math_test_suite);
-
-  // Run Matrix33 test suite
-  RUN_AND_DISPLAY_SUITE(mat33_test_suite);
-
-  // Run Matrix43 test suite
-  RUN_AND_DISPLAY_SUITE(mat43_test_suite);
-
-  // Run Plane test suite
-  RUN_AND_DISPLAY_SUITE(plane_test_suite);
-
-  // Run Sphere test suite
-  RUN_AND_DISPLAY_SUITE(sphere_test_suite);
-
-  // Run Collision test suite
-  RUN_AND_DISPLAY_SUITE(collision_test_suite);
-
-  // Run Frustum test suite
-  RUN_AND_DISPLAY_SUITE(frustum_test_suite);
-
-  // Run Vector2D test suite
-  RUN_AND_DISPLAY_SUITE(vector2d_test_suite);
-
-  // Run Vector3D test suite
-  RUN_AND_DISPLAY_SUITE(vector3d_test_suite);
-
-  // Run MatrixStack test suite
-  RUN_AND_DISPLAY_SUITE(matrix_stack_test_suite);
-
-  // Run Precision test suite
-  RUN_AND_DISPLAY_SUITE(precision_test_suite);
-
-  // Run SortOrder test suite
-  RUN_AND_DISPLAY_SUITE(sort_order_test_suite);
-
-  // Run Random test suite
-  RUN_AND_DISPLAY_SUITE(random_test_suite);
-
-  // Run Trigonometry test suite
-  RUN_AND_DISPLAY_SUITE(trigonometry_test_suite);
-
-  // Run Utils test suite
-  RUN_AND_DISPLAY_SUITE(utils_test_suite);
-
-  // Run AABB test suite
   RUN_AND_DISPLAY_SUITE(aabb_test_suite);
-
-  // // Run Memory test suite
-  //RUN_AND_DISPLAY_SUITE(memory_test_suite);
-
-  // Run Base test suite (SGL)
+  RUN_AND_DISPLAY_SUITE(angle_test_suite);
+  RUN_AND_DISPLAY_SUITE(ascii_test_suite);
   RUN_AND_DISPLAY_SUITE(base_test_suite);
-
-  // // Run Bitmap test suite
   RUN_AND_DISPLAY_SUITE(bitmap_test_suite);
-
-  // // Run Memory HWRam test suite
-  RUN_AND_DISPLAY_SUITE(memory_HWRam_test_suite);
-
-  // Run Memory LWRam test suite
-  RUN_AND_DISPLAY_SUITE(memory_LWRam_test_suite);
-
-  // // Run Memory CartRam test suite
-  RUN_AND_DISPLAY_SUITE(memory_CartRam_test_suite);
-
-  // Run DSP test suite
+  RUN_AND_DISPLAY_SUITE(cd_test_suite);
+  RUN_AND_DISPLAY_SUITE(collision_test_suite);
+  RUN_AND_DISPLAY_SUITE(cram_test_suite);
   RUN_AND_DISPLAY_SUITE(dsp_test_suite);
-
-  // Run System test suite
+  RUN_AND_DISPLAY_SUITE(frustum_test_suite);
+  RUN_AND_DISPLAY_SUITE(fxp_test_suite);
+  RUN_AND_DISPLAY_SUITE(highcolor_test_suite);
+  RUN_AND_DISPLAY_SUITE(mat33_test_suite);
+  RUN_AND_DISPLAY_SUITE(mat43_test_suite);
+  RUN_AND_DISPLAY_SUITE(math_test_suite);
+  //RUN_AND_DISPLAY_SUITE(memory_test_suite);
+  RUN_AND_DISPLAY_SUITE(memory_CartRam_test_suite);
+  RUN_AND_DISPLAY_SUITE(memory_HWRam_test_suite);
+  RUN_AND_DISPLAY_SUITE(memory_LWRam_test_suite);
+  RUN_AND_DISPLAY_SUITE(matrix_stack_test_suite);
+  RUN_AND_DISPLAY_SUITE(plane_test_suite);
+  RUN_AND_DISPLAY_SUITE(precision_test_suite);
+  RUN_AND_DISPLAY_SUITE(random_test_suite);
+  RUN_AND_DISPLAY_SUITE(sort_order_test_suite);
+  RUN_AND_DISPLAY_SUITE(sphere_test_suite);
   RUN_AND_DISPLAY_SUITE(system_test_suite);
-
-  // Run Interrupt test suite
-  RUN_AND_DISPLAY_SUITE(interrupt_test_suite);
+  RUN_AND_DISPLAY_SUITE(trigonometry_test_suite);
+  RUN_AND_DISPLAY_SUITE(utils_test_suite);
+  RUN_AND_DISPLAY_SUITE(vector2d_test_suite);
+  RUN_AND_DISPLAY_SUITE(vector3d_test_suite);
 
   // // Generate tests report
   MU_REPORT();
